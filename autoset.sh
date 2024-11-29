5b2b3c259102924008ae7d5e9768e96508783984
gx <2627403974@qq.com>
2024-11-29 21:40:50 +0800

    dotfile complete

--- autoset.sh
+++ autoset.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-ln -s ~/dotfile/.vimrc ~/.vimrc
-ln -s ~/dotfile/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfile/.bashrc ~/.bashrc
+DOTFILE_REPO_URL="https://github.com/chirouo/dotfile.git"
+DOTFILE_DIR="$HOME/dotfiles"
+
+if [[ -d ".git" ]]; then
+	echo "Had cloned dotfile's git repo.Pull the latest changes..." >&2
+	git pull
+else 
+	echo "No dotfile's repo! git clone ing..." >&2
+	git clone "$DOTFFILE_REPO_URL"
+fi
+ln -fs ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -fs ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -fs ~/dotfiles/bash/.bashrc ~/.bashrc
